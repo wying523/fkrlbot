bf8a0ded05e0e9799832c6eaba889ce15259e473
Akshay Chiwhane <achiwhane@gmail.com>
2015-06-15 10:29:29 -0400

    init commit

---
+++ bot.py
@@ -0,0 +1,14 @@
+import praw
+
+"""
+  FKRBot -- calculates the Flesch-Kincaid Reading Level
+            for submission titles in all subreddits
+  Licensed under GPLv3
+"""
+
+
+u_a = "Flesch–Kincaid Readability Bot v0.0.1 (by /u/discr3t3)"
+r = praw.Reddit(user_agent = u_a)
+
+
+
