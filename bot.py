97a9148d43b747f07befb749df62b7d2129c4b24
Akshay Chiwhane <achiwhane@gmail.com>
2015-06-16 11:03:45 -0400

    refactor bot & update config

--- bot.py
+++ bot.py
@@ -11,7 +11,8 @@ import os
 import string
 from config_bot import *
 
-
+# from http://www.howmanysyllables.com/howtocountsyllables.html
+# a decent approximation for number of syllables
 def count_syllables(word):
   vowels = ["a", "e", "i", "o", "u", "y"]
   num_vowels = 0
@@ -20,11 +21,11 @@ def count_syllables(word):
   for c in word:
     found_vowel = False
     for v in vowels:
-      if v == c && last_char_was_vowel:
+      if v == c and last_char_was_vowel:
         found_vowel = True
         last_char_was_vowel = True
         break
-      elif v == c && !last_char_was_vowel
+      elif v == c and not last_char_was_vowel:
         num_vowels = num_vowels + 1
         found_vowel = True
         last_char_was_vowel = True
@@ -32,63 +33,69 @@ def count_syllables(word):
     if not found_vowel:
       last_char_was_vowel = False
 
-  if len(word) > 2 && ("es" in word[-2:])
+  if len(word) > 2 and ("es" in word[-2:]):
     num_vowels = num_vowels - 1;
-  elif len(word) > 1 && ("e" in word[-1])
+  elif len(word) > 1 and ("e" in word[-1]):
     num_vowels = num_vowels - 1;
   return num_vowels
 
-def title_score(num_words, num_sentences, num_syllables):
-  return 206.835 - 1.015(num_words/num_sentences) - 84.6(num_syllables/num_words)
 
+def title_score(nword, nsent, nsyl):
+  return 206.835 - 1.015 * (nword / nsent) - 84.6 * (nsyl / nword)
 
-u_a = "Flesch-Kincaid Reading Level v0.0.1 by /u/__0xDEADBEEF)"
-r = praw.Reddit(user_agent = u_a)
+def main():
+  u_a = "Flesch-Kincaid Reading Level v0.0.1 by /u/__0xDEADBEEF)"
+  r = praw.Reddit(user_agent = u_a)
 
+  # see config_skel.py for username and password formatting
+  r.login(REDDIT_USERNAME, REDDIT_PASSWORD)
 
-# see config_skel.py for username and password formatting
-r.login(REDDIT_USERNAME, REDDIT_PASSWORD)
+  # stores each post replied to's unique ID in a file
+  already_done = []
 
+  subreddit = r.get_subreddit(REDDIT_SUB)
+  num_posts = 10
+  for submission in subreddit.get_hot(limit = num_posts):
+    """
+      TODO: 
+      (1) count the number of words
+      (2) count the number of sentences
+      (3) calculate the Flesch-Kincaid score
+      (4) output a sentence describing the readibility of 
+          the title
+      (5) submit the comment to all the new posts in a 
+          subreddit
+    """
+    if not submission.id in already_done:
+      already_done.insert(0, submission.id)
 
-# stores each post replied to's unique ID in a file
-# 
+      title_string = submission.title
+      title_words = title_string.split()
 
+      num_words = len(title_words)
 
-already_done = []
+      # does not account for interrobangs b/c of their infrequency
+      num_sentences = (title_string.count('.') + title_string.count('!') 
+                        + title_string.count('?'))
 
-subreddit = r.get_subreddit("news")
-num_posts = 10
-for submission in subreddit.get_hot(limit = num_posts):
-  """
-    TODO: 
-    (1) count the number of words
-    (2) count the number of sentences
-    (3) calculate the Flesch-Kincaid score
-    (4) output a sentence describing the readibility of 
-        the title
-    (5) submit the comment to all the new posts in a 
-        subreddit
-  """
-  title_string = submission.title
-  title_arr = title_string.split()
+      # if the title does not have any punctuation, 
+      # then consider it as 1 sentence         
+      if num_sentences < 1:
+        num_sentences = 1
 
-  num_words = len(title_string.split())
+      total_syllables = 0
 
-  # does not account for interrobangs
-  num_sentences = (title_string.count('.') + title_string.count('!') 
-                    + title_string.count('?'))
-  # if the title does not have any punctuation, then consider it as 1 sentence         
-  if num_sentences < 1:
-    num_sentences = 1
+      for i in title_words:
+        # regex to strip most common punctuation or special characters
+        i = re.sub("[\.\t\,\:;\(\)\.\-\--]", "", i, 0, 0)
+        i = i.lower()
+        total_syllables = total_syllables + count_syllables(i)
 
-  total_syllables = 0
+      score = title_score(num_words, num_sentences, total_syllables)
+      print score
 
-  for i in title_string.split():
-    i = i.translate(None, "?.!/;:-")
-    i = i.lower()
-    total_syllables = total_syllables + count_syllables(i)
-
-  score = title_score(num_words, num_sentences, num_syllables)
+if __name__ == "__main__":
+  main()
 
 
 
--- config_skel.py
+++ config_skel.py
@@ -1,2 +1,3 @@
 REDDIT_USERNAME = "" # please enter your Reddit username here
 REDDIT_PASSWORD = "" # please enter your Reddit password here
+REDDIT_SUB = "" # add the subreddit you want to analyze here 
